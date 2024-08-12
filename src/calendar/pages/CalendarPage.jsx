e2af4efc4abbc08cf2c766f4089ea076efbe21b2
JuanCM <juancampuzano2356@gmail.com>
2024-08-12 16:10:57 -0500

    Se finaliza la clase 353

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,56 @@
-import { NavBar } from "../"
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css'; 
+import { addHours } from 'date-fns';
+
+import { CalendarEvent, NavBar } from "../";
+import { localizer, getMessagesEs } from '../../helpers';
+
+
+const events = [{
+  title: 'Cumpleaños del jefe',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Fernando'
+  }
+}]
 
 export const CalendarPage = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return{
+
+    }
+
+  }
   return (
     <>
       <NavBar />
+
+      <Calendar
+      culture='es'
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessagesEs()}
+        eventPropGetter={ eventStyleGetter }
+        components={{
+          event: CalendarEvent
+        }}
+      />
+
     </>
   )
 }
